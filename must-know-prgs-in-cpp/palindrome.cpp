91d440de4e1472c9783d7b54f203646ee430b587
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-07-15 13:47:57 +0530

    Add files via upload

---
+++ must-know-prgs-in-cpp/palindrome.cpp
@@ -0,0 +1,20 @@
+    #include <iostream>  
+      
+    int main()  
+    {  
+      int n,r,sum=0,temp;    
+      std:: cout<<"Enter the Number:-"<<std::endl;    
+      std::cin>>n;    
+     temp=n;    
+     while(n>0)    
+    {    
+     r=n%10;    
+     sum=(sum*10)+r;    
+     n=n/10;    
+    }    
+    if(temp==sum)    
+    std::cout<<"Number is Palindrome."<<std::endl;    
+    else    
+    std::cout<<"Number is not Palindrome."<<std::endl;   
+      return 0;  
+    }  
