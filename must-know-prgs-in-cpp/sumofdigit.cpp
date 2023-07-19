1fc9bd1fe033dbe386cd0c3d1dfb6f0082c902eb
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-07-19 22:24:59 +0530

    Add files via upload

---
+++ must-know-prgs-in-cpp/sumofdigit.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main(){
+
+
+	int n,m;
+	int sum=0;
+	std::cout<<"enter the number:"<<std::endl;
+	std::cin>>n;
+	while(n>0){
+                m=n%10;
+		sum=sum+m;
+		n=n/10;
+	}
+	std::cout<<"sum is:"<<sum<<std::endl;
+	return 0;
+}
+
