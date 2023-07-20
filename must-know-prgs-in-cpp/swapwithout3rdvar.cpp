1f3ba5244cf09fbd448e132a6a3d6bb7e3c7be70
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-07-20 14:11:20 +0530

    Add files via upload

---
+++ must-know-prgs-in-cpp/swapwithout3rdvar.cpp
@@ -0,0 +1,19 @@
+    #include <iostream>  
+      
+    int main()  
+    {  
+    int a, b;
+    std::cout<<"enter 1st num:"<<std::endl;
+    std::cin>>a;
+
+    std::cout<<"enter 2nd num:"<<std::endl;
+    std::cin>>b;
+
+        
+    std::cout<<"Before swap a= "<<a<<" b= "<<b<<std::endl;      
+    a=a+b; //a=15 (5+10)    
+    b=a-b; //b=5 (15-10)    
+    a=a-b; //a=10 (15-5)    
+    std:: cout<<"After swap a= "<<a<<" b= "<<b<<std::endl;      
+    return 0;  
+    }  
