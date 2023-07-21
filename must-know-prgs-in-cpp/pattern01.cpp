149eec225e5bf81b1f89d789bb318cd75703fd9f
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-07-21 13:53:45 +0530

    pattern01.cpp

--- must-know-prgs-in-cpp/pattern01.cpp
+++ must-know-prgs-in-cpp/pattern01.cpp
@@ -1,11 +1,9 @@
     #include <iostream>  
     
-    int main()  
-    {  
+    int main(){  
      char ch='A';    
         int i, j, k, m;    
-        for(i=1;i<=5;i++)    
-        {    
+        for(i=1;i<=5;i++){    
             for(j=5;j>=i;j--)    
                 std::cout<<" ";    
             for(k=1;k<=i;k++)    
@@ -13,7 +11,7 @@
                 ch--;    
             for(m=1;m<i;m++)    
                 std::cout<<--ch;    
-	    std:: cout<<"\n";    
+	        std:: cout<<"\n";    
             ch='A';    
         }    
     return 0;  
