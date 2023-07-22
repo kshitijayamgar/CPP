a9d6f234e8f310e6ab5bbaee6afced709c550d19
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-07-22 17:28:44 +0530

    ptrn01.cpp

---
+++ patterns/ptrn01.cpp
@@ -0,0 +1,17 @@
+ #include <iostream>
+int main(){
+     char ch='A';
+        int i, j, k, m;
+        for(i=1;i<=5;i++){
+            for(j=5;j>=i;j--)
+                std::cout<<" ";
+            for(k=1;k<=i;k++)
+                std::cout<<ch++;
+                ch--;
+            for(m=1;m<i;m++)
+                std::cout<<--ch;
+            std:: cout<<"\n";
+            ch='A';
+        }
+    return 0;
+    }
