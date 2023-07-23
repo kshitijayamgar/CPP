b035212145dfdda5c39810737cb64f6328981bee
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-07-23 13:33:47 +0530

    Add files via upload

---
+++ patterns/pattern02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main() {
+  // size of the square
+  int size = 5;
+  // outer loop
+  for (int i = 0; i < size; i++) {
+    // inner loop
+    for (int j = 0; j < size; j++) {
+	    std::cout << "*";
+    }
+    std:: cout << "\n";
+  }
+  return 0;
+}
