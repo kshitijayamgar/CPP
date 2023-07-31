f04e0ffa2b2863be1f919cc13caf32701d49f471
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-07-31 15:01:20 +0530

    Add files via upload

---
+++ patterns/ptrn11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    int n = 5;
+ 
+    // looping rows
+    for (int i = n; i > 0; i--) {
+        for (int j = 1; j <= n; j++) // looping columns
+        {
+            if (j >= i) {
+		    std::cout << "* ";
+            }
+            else {
+		    std:: cout << "  ";
+            }
+        }
+	std::cout << std::endl;
+    }
+    return 0;
+}
