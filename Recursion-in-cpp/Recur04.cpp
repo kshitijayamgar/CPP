0a4b514f06b1b67813c17f54ff7016b76413f467
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-08-20 15:08:11 +0530

    Add files via upload

---
+++ Recursion-in-cpp/Recur04.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// Recursive function to calculate the factorial
+int factorial(int n) {
+  // Factorial of 0 or 1 is 1
+  if (n == 0 || n == 1)
+    return 1;
+
+  // Recursive case: factorial of n is n multiplied by factorial of (n-1)
+  return n * factorial(n - 1);
+}
+
+int main() {
+  int num;
+  std::cout << "Input a number: ";
+  std::cin >> num;
+
+  // Calculate the factorial using recursion
+  int fact = factorial(num);
+
+  std::cout << "Factorial of " << num << " is: " << fact << std::endl;
+
+  return 0;
+}
+
